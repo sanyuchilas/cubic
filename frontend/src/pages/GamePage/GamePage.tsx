f558e998c749c531c93c4ad92f01006379f71c45
sanyuchilas <sanyuchilas@gmail.com>
2023-02-25 21:06:08 +0300

    add: successfuly loading .glb model

--- frontend/src/pages/GamePage/GamePage.tsx
+++ frontend/src/pages/GamePage/GamePage.tsx
@@ -1,20 +1,28 @@
-import { TrackballControls, OrbitControls } from '@react-three/drei';
+import { TrackballControls, OrbitControls, useGLTF } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import React from 'react';
+import { FisrtLevel } from '../../components/models/FirstLevel/FirstLevel';
 import TestBox from '../../components/TestBox/TestBox';
+import styles from './GamePage.module.scss'
 
 const GamePage = () => {
-  const isReverse = true;
+
   return (
-    <Canvas style={{height: '100vh', width: '100vw'}}>
-      <ambientLight />
-      <pointLight position={[15, 15, 90]} />
-      <TrackballControls noRotate={true} noPan={true}/>
-      <OrbitControls enableZoom ={false}/>
-      {isReverse && <TestBox position={[0, 0, 0]} />}
-      <TestBox position={[0, 2, 0]} />
-      <TestBox position={[0, -2, 0]}/>
-    </Canvas>
+    // <Canvas style={{height: '100vh', width: '100vw'}}>
+    //   <ambientLight />
+    //   {isReverse && <TestBox position={[0, 0, 0]} />}
+    //   <TestBox position={[0, 2, 0]} />
+    //   <TestBox position={[0, -2, 0]}/>
+    // </Canvas>
+    <div className={styles.container}>
+      <Canvas style={{height: '100%', width: '100%'}}>
+        <ambientLight />
+        <pointLight position={[15, 15, 90]} />
+        <TrackballControls noRotate={true} noPan={true}/>
+        <OrbitControls enableZoom ={false}/>
+        <FisrtLevel/>
+      </Canvas>
+    </div>
   );
 };
 
