a23c6ad932f0783abad6f65b66cd65e68e270f41
sanyuchilas <sanyuchilas@gmail.com>
2023-02-26 05:21:29 +0300

    add: fully add level_1 in project

---
+++ frontend/src/components/FirstLevel/FirstLevel.tsx
@@ -0,0 +1,78 @@
+import { useGLTF } from '@react-three/drei'
+import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader'
+//@ts-ignore
+import level_1 from './../../assets/3D/level_1.1.gltf'
+
+type GLTFResult = GLTF & {
+  nodes: Record<string, THREE.Mesh>
+  materials: Record<string, THREE.MeshStandardMaterial>
+}
+
+export function FisrtLevel() {
+  const { nodes, materials } = useGLTF(level_1) as GLTFResult
+
+  return (
+    <group dispose={null}>
+      {/* <mesh geometry={nodes.plane.geometry} material={materials.plane} position={[0, -0.21, 0]} scale={94.98}/> */}
+      <mesh geometry={nodes['3_cube'].geometry} castShadow material={materials.object} position={[5.5, 0.5, 1.5]}>
+        <meshLambertMaterial wireframe color={'black'}/>
+      </mesh>
+      <mesh geometry={nodes['3_cube'].geometry} castShadow material={materials.object} position={[5.5, 0.5, 1.5]}/>
+      <group position={[4.13, 2.86, -2.17]}>
+        <mesh geometry={nodes.screen_3.geometry} material={materials.frame}>
+          <meshLambertMaterial wireframe color={'black'}/>
+        </mesh>
+        <mesh geometry={nodes.screen_3.geometry} material={materials.frame}/>
+        <mesh geometry={nodes.screen_3_1.geometry} material={materials.screen} />
+      </group>
+      <group position={[4.12, 1.43, -2.86]}>
+        <mesh geometry={nodes['3_btns'].geometry} material={materials.btn_green} />
+        <mesh geometry={nodes['3_btns_1'].geometry} material={materials.btn_yellow} />
+        <mesh geometry={nodes['3_btns_2'].geometry} material={materials.btn_red} />
+      </group>
+      <mesh geometry={nodes['3_carrie'].geometry} material={materials.boxes} position={[4.08, 1.19, -1.57]} />
+      <mesh geometry={nodes['3_export_wires'].geometry} material={materials.wires} position={[4.16, 1.72, -0.03]} rotation={[1.5, 0, -Math.PI / 2]} />
+      <group position={[4.08, 1.19, -1.57]}>
+        <mesh geometry={nodes['3_handle_1'].geometry} material={materials.richag} />
+        <mesh geometry={nodes['3_handle_2'].geometry} material={materials.pipka}>
+        </mesh>
+      </group>
+      <mesh geometry={nodes['2_freeze'].geometry} castShadow material={materials.freeze} position={[0.5, 0.5, -3.5]}>
+        <meshLambertMaterial wireframe color={'#8DA7D6'}/>
+      </mesh>
+      <group position={[-0.5, 0.59, -0.5]} rotation={[-Math.PI, Math.PI / 4, -1.2]}>
+        <mesh geometry={nodes['2_handle_1'].geometry} material={materials.richag} />
+        <mesh geometry={nodes['2_handle_2'].geometry} material={materials.pipka} />
+      </group>
+      <mesh geometry={nodes['2_contramotor'].geometry} castShadow material={materials.boxes} position={[-0.5, 0.59, -0.5]} rotation={[Math.PI, Math.PI / 4, -1.2]} />
+      <group position={[-5.95, 2.49, 0.04]}>
+        <mesh geometry={nodes.screen_1.geometry} castShadow material={materials.screen} />
+        <mesh geometry={nodes.screen_1_1.geometry} castShadow material={materials.frame}>
+          <meshLambertMaterial wireframe color={'black'}/>
+        </mesh>
+        <mesh geometry={nodes.screen_1_1.geometry} castShadow material={materials.frame}/>
+      </group>
+      <mesh geometry={nodes['1_pc'].geometry} castShadow material={materials.pc} position={[-2.5, 0.5, -1.5]}>
+        <meshLambertMaterial wireframe color={'black'}/>
+      </mesh>
+      <group rotation={[0, 0, -Math.PI / 2]} position={[-1.12, -1.12, 0]}>
+        <mesh geometry={nodes['1_contramotor'].geometry} castShadow material={materials.boxes} position={[-0.73, -0.62, 1.5]} />
+        <group position={[-1.5, 0.59, 1.5]} rotation={[0, Math.PI / 4, 1.94]}>
+          <mesh geometry={nodes['1_handle_1'].geometry} material={materials.richag} />
+          <mesh geometry={nodes['1_handle_2'].geometry} material={materials.pipka} />
+        </group>
+      </group>
+      <mesh geometry={nodes.room_1.geometry} receiveShadow material={materials.room}>
+        <meshLambertMaterial wireframe color={'black'}/>
+      </mesh>
+      <mesh geometry={nodes.room_1.geometry} receiveShadow material={materials.room}>
+        <meshLambertMaterial/>
+      </mesh>
+      <mesh geometry={nodes.room_2.geometry} material={materials.window_1} />
+      <mesh geometry={nodes.room_3.geometry} material={materials.window_2} />
+      <mesh geometry={nodes.room_4.geometry} material={materials.window_between} />
+    </group>
+  )
+}
+
+useGLTF.preload(level_1)
--- frontend/src/pages/GamePage/GamePage.tsx
+++ frontend/src/pages/GamePage/GamePage.tsx
@@ -1,27 +1,77 @@
-import { TrackballControls, OrbitControls, useGLTF } from '@react-three/drei';
+import { OrbitControls, TrackballControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
-import React from 'react';
-import { FisrtLevel } from '../../components/models/FirstLevel/FirstLevel';
-import TestBox from '../../components/TestBox/TestBox';
-import styles from './GamePage.module.scss'
+import { PerspectiveCamera } from 'three';
+import { FisrtLevel } from '../../components/FirstLevel/FirstLevel';
+import styles from './GamePage.module.scss';
 
 const GamePage = () => {
+  const camera = new PerspectiveCamera(50, 2, 0.1, 1000)
+
+  camera.position.set(7.58398, 4.99278, 17.957)
 
   return (
-    // <Canvas style={{height: '100vh', width: '100vw'}}>
-    //   <ambientLight />
-    //   {isReverse && <TestBox position={[0, 0, 0]} />}
-    //   <TestBox position={[0, 2, 0]} />
-    //   <TestBox position={[0, -2, 0]}/>
-    // </Canvas>
     <div className={styles.container}>
-      <Canvas style={{height: '100%', width: '100%'}}>
-        <ambientLight />
-        <pointLight position={[15, 15, 90]} />
-        <TrackballControls noRotate={true} noPan={true}/>
-        <OrbitControls enableZoom ={false}/>
+      <Canvas 
+        style={{height: '100%', width: '100%'}}
+        camera={camera}
+        shadows
+      > 
+        <ambientLight color='#666' intensity={.75}/>
+        {/* <pointLight
+          position={[5.13702, 4.61732, -2.5]}
+          intensity={1}
+          power={15}
+          shadow-mapSize={512}
+          castShadow
+          name='light_3'
+          distance={12}
+          color={'white'}
+        /> */}
+        <pointLight
+          position={[2.44001, 7.9474, 7.81545]}
+          intensity={2}
+          power={50}
+          shadow-mapSize={512}
+          castShadow
+          name='ligth_4'
+          distance={16}
+          color={'white'}
+        />
+        {/* <pointLight
+          position={[-3.38417, 4.62732, 0.203768]}
+          intensity={1}
+          power={10}
+          shadow-mapSize={512}
+          castShadow
+          name='ligth_1'
+          distance={7}
+          color={'white'}
+        /> */}
+        {/* <pointLight
+          position={[1.63165, 4.62732, 0.032774]}
+          intensity={1}
+          power={10}
+          shadow-mapSize={512}
+          castShadow
+          name='ligth_2'
+          distance={7}
+          color={'white'}
+        /> */}
+        <TrackballControls
+          noRotate={true}
+          noPan={true}
+          maxDistance={20}
+          minDistance={5}
+          target={[-0.25, 2.5, 0]}
+        />
+        <OrbitControls
+          enableZoom ={false}
+          target={[-0.25, 2.5, 0]}
+        />
         <FisrtLevel/>
+        <axesHelper args={[5]}/>
       </Canvas>
+      <div className={styles.panel}></div>
     </div>
   );
 };
